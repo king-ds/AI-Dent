3d686e3bfc12ba09c4c8136ee8fcbae1aa503e92
king-ds <ricardo.calura29@gmail.com>
2019-12-20 10:26:42 +0800

    add instructor viewer

--- src/app/members/instructor-viewer/instructor-viewer.page.ts
+++ src/app/members/instructor-viewer/instructor-viewer.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from './../../services/api.service';
+import { Observable } from 'rxjs';
+import { StorageService } from './../../services/storage.service';
 
 @Component({
   selector: 'app-instructor-viewer',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructorViewerPage implements OnInit {
 
-  constructor() { }
+  clinician : any;
+  instructors : Observable<any>;
+  searchTerm : string = '';
+  debouncer : any;
+  loader : boolean;
+  empty : boolean;
+
+  constructor(private apiService : ApiService,
+              private storageService : StorageService) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.loader = true;
+    this.storageService.getObject('clinician').then((result) => {
+      this.clinician = result;
+      console.log(this.clinician);
+      
+      this.debouncer = setTimeout(() => {
+          this.checkInstructorList();
+          this.loader = false;
+        }, 2000)
+      });
+  }
+
+  checkInstructorList(){
+    this.apiService.getInstructorList().subscribe(val => {
+      if(val == ''){
+        this.empty = true;
+      }else{
+        this.empty = false;
+        this.instructors = this.apiService.getInstructorList();
+      }
+    })
+  }
+
+  searchChanged(){
+    this.instructors = this.apiService.searchPatient(this.searchTerm);
   }
 
 }
